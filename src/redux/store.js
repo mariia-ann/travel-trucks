988091a899f8d6c551c9e0f5e65463b1efa2db26
Mariia <myshyck@gmail.com>
2025-08-21 13:54:03 +0300

    feat(filter/favorite): add location filter, refactor favorites logic

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { filterReducer } from './filters/slice.js';
-import { campersReducer } from './campers/slice.js';
+import { configureStore } from "@reduxjs/toolkit";
+import { filterReducer } from "./filters/slice.js";
+import { campersReducer } from "./campers/slice.js";
+import favoritesReducer from "./favorites/slice";
 
 export const store = configureStore({
   reducer: {
     campers: campersReducer,
     filters: filterReducer,
+    favorites: favoritesReducer,
   },
 });
