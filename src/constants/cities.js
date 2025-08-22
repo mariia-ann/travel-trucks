d0d5d3116dc0b1a106c66cd356cfaf71e952eb76
Mariia <myshyck@gmail.com>
2025-08-22 10:41:58 +0300

    refactor(filter): move cities to constants

---
+++ src/constants/cities.js
@@ -0,0 +1,9 @@
+export const cities = [
+  { key: "kyiv", label: "Ukraine, Kyiv" },
+  { key: "poltava", label: "Ukraine, Poltava" },
+  { key: "dnipro", label: "Ukraine, Dnipro" },
+  { key: "odesa", label: "Ukraine, Odesa" },
+  { key: "kharkiv", label: "Ukraine, Kharkiv" },
+  { key: "sumy", label: "Ukraine, Sumy" },
+  { key: "lviv", label: "Ukraine, Lviv" },
+];
